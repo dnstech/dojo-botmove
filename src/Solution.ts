a1087f6cf6e1e440d56c396fb90fc33e5d1a84c2
Joshua Webb <joshua.webb@dnstech.com.au>
2022-08-22 14:00:20 +1000

    Implement keyboard handling

--- src/Solution.ts
+++ src/Solution.ts
@@ -1,9 +1,45 @@
-import { IRoom, IRobot } from "./bot/bot";
+import {IRoom, IRobot, Direction} from "./bot/bot";
 
 // intermediate challenge
 export const keyHandler = (key: string, robot: IRobot) => {
-  // your implementation here
+  var desiredDirection: Direction | null = null;
+  switch (key) {
+    case 'w': case 'ArrowUp'   : desiredDirection = 'N'; break;
+    case 's': case 'ArrowDown' : desiredDirection = 'S'; break;
+    case 'a': case 'ArrowLeft' : desiredDirection = 'W'; break;
+    case 'd': case 'ArrowRight': desiredDirection = 'E'; break;
+  }
+
+  // Keyboard currently only controls movement.
+  if (desiredDirection == null)
+    return false;
+
+  // Figure out which direction will get us closer to our desired direction in order to turn optimally.
+  const TURN_AMOUNT_DEGREES = 90;
+  let currentAngle = getAngle(robot.state.direction);
+  let desiredAngle = getAngle(desiredDirection);
+  let leftDiff  = Math.abs(currentAngle - TURN_AMOUNT_DEGREES - desiredAngle) % 360;
+  let rightDiff = Math.abs(currentAngle + TURN_AMOUNT_DEGREES - desiredAngle) % 360;
+  while (robot.state.direction !== desiredDirection) {
+    if (leftDiff < rightDiff) {
+      robot.turnLeft(true);
+    } else {
+      robot.turnRight(true);
+    }
+  }
+
+  robot.move(true);
+  return true;
 };
+
+function getAngle (direction: Direction) {
+  switch (direction) {
+    case "N": return 0;
+    case "E": return 90;
+    case "S": return 180;
+    case "W": return 270;
+  }
+}
 
 // advanced challenge
 export const getMeToTheSmileyFace = (room: IRoom, robot: IRobot) => {
