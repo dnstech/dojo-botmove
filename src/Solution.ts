beb4fe19892324f6fdbde039fb74df11897fe562
chengkungui <chengkun.gui@dnstech.com.au>
2022-08-22 14:27:50 +1000

    dojo-botmove implement keyHandler

--- src/Solution.ts
+++ src/Solution.ts
@@ -1,8 +1,21 @@
-import { IRoom, IRobot } from "./bot/bot";
+import { stat } from "fs";
+import { IRoom, IRobot, IRobotState, Direction } from "./bot/bot";
 
 // intermediate challenge
 export const keyHandler = (key: string, robot: IRobot) => {
+
   // your implementation here
+  if (key === 'ArrowRight') {
+    robot.turnRight(true);
+  } else if (key === 'ArrowLeft') {
+    robot.turnLeft(true);
+  } else if (key === 'ArrowUp') {
+    robot.move(true)
+  } else if (key === 'ArrowDown') {
+    robot.turnRight(true);
+    robot.turnRight(true);
+    robot.move(true);
+  }
 };
 
 // advanced challenge
@@ -10,3 +23,13 @@ export const getMeToTheSmileyFace = (room: IRoom, robot: IRobot) => {
   // your implementation here
   alert('Please implement Solution.ts');
 };
+
+type TurnDirection = 'L' | 'R';
+
+function getTurnState(state: IRobotState, turn: TurnDirection): IRobotState {
+  const directions : Direction[] = ['N', 'E', 'S', 'W'];
+  let nextDirection = directions[(directions.indexOf(state.direction) + turn === 'R' ? 1 : -1) % 4];
+  let turnState = { ...state };
+  turnState.direction = nextDirection;
+  return turnState;
+}
