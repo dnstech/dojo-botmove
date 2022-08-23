177035984df33054a6b8fc346df8a77c48738abe
jereth.kok <jereth.kok@dnstech.com.au>
2022-08-23 15:28:54 +1000

    intermediate challenge Solution

--- src/Solution.ts
+++ src/Solution.ts
@@ -3,6 +3,21 @@ import { IRoom, IRobot } from "./bot/bot";
 // intermediate challenge
 export const keyHandler = (key: string, robot: IRobot) => {
   // your implementation here
+
+  switch (key) {
+    case "ArrowUp":
+      robot.move(true);
+      break;
+
+    case "ArrowLeft":
+      robot.turnLeft(true);
+      break;
+
+    case "ArrowRight":
+      robot.turnRight(true);
+      break;
+  }
+
 };
 
 // advanced challenge
